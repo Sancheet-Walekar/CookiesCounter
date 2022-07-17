a7bd2968d34b42fa9dc34599d2a5c0c2a5babdae
18sancheet <sancheetwalekar@gmail.com>
2022-07-18 00:11:25 +0530

    first commit

---
+++ (3) CookieCounter/cookies/cookies.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="cookies.aspx.cs" Inherits="cookies.cookies" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblvisit" runat="server" Font-Size="X-Large"></asp:Label>
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnsubmit" runat="server" Font-Size="X-Large" OnClick="btnsubmit_Click" Text="Increment" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ (3) CookieCounter/cookies/cookies.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cookies
+{
+    public partial class cookies : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Cookies["cooks"] != null)
+            {
+                int fetchctr = int.Parse(Request.Cookies["cooks"].Value);
+                fetchctr++;
+                lblvisit.Text = "This is your Visit no:-" + fetchctr;
+                HttpCookie c1 = new HttpCookie("cooks", fetchctr.ToString());
+                Response.Cookies.Add(c1);
+            }
+            else
+            {
+                int ctr = 1;
+                HttpCookie c1 = new HttpCookie("cooks", ctr.ToString());
+                lblvisit.Text = "This is your first cookie";
+                Response.Cookies.Add(c1);
+            }
+
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+    
+        }
+    }
+}
